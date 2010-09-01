a5c66ba313a0d2387f9504a321e3ea1099978643
IanWraith <ianwraith@gmail.com>
2010-09-01 08:38:49 +0100

    Add the DMR Frame Sync sequences.

--- src/com/dmr/DMRDecode.java
+++ src/com/dmr/DMRDecode.java
@@ -57,6 +57,8 @@ public class DMRDecode {
 	private int sidx=0;
 	private int ssize=36;
 	private int mod_threshold=26;
+	private static final int DMR_DATA_SYNC[]={3,1,3,3,3,3,1,1,1,3,3,1,1,3,1,1,3,1,3,3,1,1,3,1};
+	private static final int DMR_VOICE_SYNC[]={1,3,1,1,1,1,3,3,3,1,1,3,3,1,3,3,1,3,1,1,3,3,1,3};
 
 	public static void main(String[] args) {
 		theApp=new DMRDecode();
