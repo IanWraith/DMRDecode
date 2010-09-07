1a17618893f49df284a5e1c67e733e572e4bd0d8
IanWraith <ianwraith@gmail.com>
2010-09-07 14:54:37 +0100

    Add code for de-interleaving the CACH.

--- src/com/dmr/DMRDataDecode.java
+++ src/com/dmr/DMRDataDecode.java
@@ -3,27 +3,78 @@ package com.dmr;
 public class DMRDataDecode {
 	private int dibit_buf[]=new int[132];
 	private String line[]=new String[10];
-	private Boolean inverted_dmr;
+	private boolean inverted;
+	private boolean at;
+	private boolean channel;
+	private int lcss;
 	
 	public String[] decode (String timeStamp,int[] buf,boolean inv)	{
 		int i,dibit,currentslot;
 		int cachdata[]=new int[13];
 		dibit_buf=buf;
 		line[0]=timeStamp+" DMR Data Frame";
-		inverted_dmr=inv;
-		line[1]="CACH ";
-		
+		inverted=inv;
 		// CACH decode
-		for (i=0;i<12;i++)	{
-			dibit=dibit_buf[i];
-		    if (inverted_dmr==true) dibit=(dibit^2);
-		    cachdata[i]=dibit;
-		    if (i==2) currentslot=(1&(dibit>>1));    
-		    }
-		  cachdata[12]=0;
+		decodeCACH();
 
 		
 		return line;
+	}
+	
+	// De-interleave , CRC check and decode the CACH
+	private boolean decodeCACH ()	{
+		boolean rawdataCACH[]=new boolean[24];
+		boolean dataCACH[]=new boolean[24];
+		boolean tact[]=new boolean[7];
+		final int[]interleaveCACH={0,17,1,2,3,18,4,5,6,19,7,20,8,9,10,21,11,12,13,22,14,15,16,23};
+		int a,b=0,r;
+		// Convert from dibit into boolean
+		for (a=0;a<12;a++)	{
+			r=(1&(dibit_buf[a]>>1));
+			if (r==0) rawdataCACH[b]=false;
+			 else rawdataCACH[b]=true;
+			b++;
+			r=1&dibit_buf[a];
+			if (r==0) rawdataCACH[b]=false;
+			 else rawdataCACH[b]=true;
+			b++;
+		}
+		// De-interleave
+		for (a=0;a<24;a++)	{
+			r=interleaveCACH[a];
+			dataCACH[a]=rawdataCACH[r];
+		}
+		
+		// Display for diagnosic purposes
+		line[1]="CACH : ";
+		for (a=0;a<24;a++)	{
+			if (dataCACH[a]==false) line[1]=line[1]+"0";
+			 else  line[1]=line[1]+"1";
+		}
+		
+		// TACT
+		// Extract the TACT and error check it
+		for (a=23;a>16;a--)	{
+			tact[a-17]=rawdataCACH[a];			
+		}
+		
+		line[2]="TACT : ";
+		for (a=0;a<7;a++)	{
+			if (tact[a]==false) line[2]=line[2]+"0";
+			 else  line[2]=line[2]+"1";
+		}
+		
+		// Don't display anything for now
+		line[1]=null;
+		line[2]=null;
+		
+		return true;
+	}
+	
+	// Error check the CACH TACT
+	public boolean errorCheckHamming743(boolean in[])	{
+		
+		return true;
 	}
 	
 }
