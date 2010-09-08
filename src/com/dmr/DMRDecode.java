8083601ab7c3bf3e2e9f061dfd75e15d8ecf9db9
IanWraith <ianwraith@gmail.com>
2010-09-08 13:07:45 +0100

    Remove the ssize variable.

--- src/com/dmr/DMRDecode.java
+++ src/com/dmr/DMRDecode.java
@@ -58,7 +58,6 @@ public class DMRDecode {
 	private int minref=-12000;
 	private int lastsynctype=-1;
 	private int symbolcnt=0;
-	private int ssize=36;
 	private static final int DMR_DATA_SYNC[]={3,1,3,3,3,3,1,1,1,3,3,1,1,3,1,1,3,1,3,3,1,1,3,1};
 	private static final int DMR_VOICE_SYNC[]={1,3,1,1,1,1,3,3,3,1,1,3,3,1,3,3,1,3,1,1,3,3,1,3};
 	private int carrier=0;
