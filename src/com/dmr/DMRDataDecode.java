e81187143614d4ecdf010e1556feddb648b348c1
IanWraith <ianwraith@gmail.com>
2010-10-16 21:43:55 +0100

    Reverse the CACH interleave sequence.

--- src/com/dmr/DMRDataDecode.java
+++ src/com/dmr/DMRDataDecode.java
@@ -26,7 +26,8 @@ public class DMRDataDecode {
 		boolean rawdataCACH[]=new boolean[24];
 		boolean dataCACH[]=new boolean[24];
 		boolean tact[]=new boolean[7];
-		final int[]interleaveCACH={0,17,1,2,3,18,4,5,6,19,7,20,8,9,10,21,11,12,13,22,14,15,16,23};
+		//final int[]interleaveCACH={0,17,1,2,3,18,4,5,6,19,7,20,8,9,10,21,11,12,13,22,14,15,16,23};	
+		final int[]interleaveCACH={23,16,15,14,22,13,12,11,21,10,9,8,20,7,19,6,5,4,18,3,2,1,17,0};
 		int a,b=0,r;
 		// Convert from dibit into boolean
 		for (a=0;a<12;a++)	{
@@ -49,7 +50,7 @@ public class DMRDataDecode {
 		line[1]="CACH : ";
 		for (a=0;a<24;a++)	{
 			if (dataCACH[a]==false) line[1]=line[1]+"0";
-			 else  line[1]=line[1]+"1";
+			 else line[1]=line[1]+"1";
 		}
 		
 		// TACT
