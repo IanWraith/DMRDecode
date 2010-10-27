5b900e189e0d97a97cc385e5b876836366eddd1b
IanWraith <ianwraith@gmail.com>
2010-10-27 12:47:49 +0100

    Add the calcGolay208 method to generate all 256 valid values of the Golay(20,8) system.
    Done while in the Canters canteen at Bishop Burton College (27th Oct 2010)

--- src/com/dmr/SlotType.java
+++ src/com/dmr/SlotType.java
@@ -72,6 +72,62 @@ public class SlotType {
 		return res;
 	}
 	
+	// Code to calculate all valid values for Golay (20,8)
+	void calcGolday208 ()	{
+		boolean d[]=new boolean[8];
+		boolean p[]=new boolean[12];
+		int value[]=new int[256];
+		int a;
+		// Run through all possible 8 bit values
+		for (a=0;a<256;a++){
+			// Convert to binary
+			if ((a&127)>0) d[0]=true;
+			else d[0]=false;
+			if ((a&64)>0) d[1]=true;
+			else d[1]=false;
+			if ((a&32)>0) d[2]=true;
+			else d[2]=false;
+			if ((a&16)>0) d[3]=true;
+			else d[3]=false;
+			if ((a&8)>0) d[4]=true;
+			else d[4]=false;
+			if ((a&4)>0) d[5]=true;
+			else d[5]=false;
+			if ((a&2)>0) d[6]=true;
+			else d[6]=false;
+			if ((a&1)>0) d[7]=true;
+			else d[7]=false;
+			// Calculate the parity bits
+			p[0]=d[1]^d[4]^d[5]^d[6]^d[7];
+			p[1]=d[1]^d[2]^d[4];
+			p[2]=d[0]^d[2]^d[3]^d[5];
+			p[3]=d[0]^d[1]^d[3]^d[4]^d[6];
+			p[4]=d[0]^d[1]^d[2]^d[4]^d[5]^d[7];
+			p[5]=d[0]^d[2]^d[3]^d[4]^d[7];
+			p[6]=d[3]^d[6]^d[7];
+			p[7]=d[0]^d[1]^d[5]^d[6];
+			p[8]=d[0]^d[1]^d[2]^d[6]^d[7];
+			p[9]=d[2]^d[3]^d[4]^d[5]^d[6];
+			p[10]=d[0]^d[3]^d[4]^d[5]^d[6]^d[7];
+			p[11]=d[1]^d[2]^d[3]^d[5]^d[7];
+			// Shift the value 12 times to the left
+			value[a]=a<<12;
+			if (p[0]==true) value[a]=value[a]+2048;
+			if (p[1]==true) value[a]=value[a]+1024;
+			if (p[2]==true) value[a]=value[a]+512;
+			if (p[3]==true) value[a]=value[a]+256;
+			if (p[4]==true) value[a]=value[a]+128;
+			if (p[5]==true) value[a]=value[a]+64;
+			if (p[6]==true) value[a]=value[a]+32;
+			if (p[7]==true) value[a]=value[a]+16;
+			if (p[8]==true) value[a]=value[a]+8;
+			if (p[9]==true) value[a]=value[a]+4;
+			if (p[10]==true) value[a]=value[a]+2;
+			if (p[11]==true) value[a]=value[a]+1;
+		}
+		// Just something to break on !
+		a++;
+	}
 	
 	
 }
