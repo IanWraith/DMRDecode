93e38036b1d635e7c638a60610ebecbfd3a1f8a7
IanWraith <ianwraith@gmail.com>
2011-01-28 11:31:57 +0000

    Change the code that the binary from unknown CSBKs is displayed on the same like as everything else.

--- src/com/dmr/CSBK.java
+++ src/com/dmr/CSBK.java
@@ -61,14 +61,13 @@ public class CSBK {
 	// Handle unknown CSBK types
 	private void unknownCSBK (int csbko,int fid,boolean bits[])	{
 		int a;
-		display[0]="<b>Unknown CSBK : CSBKO="+Integer.toString(csbko)+" + FID="+Integer.toString(fid)+"</b>";
+		display[0]="<b>Unknown CSBK : CSBKO="+Integer.toString(csbko)+" + FID="+Integer.toString(fid)+" ";
 		// Display the binary
-		display[1]="<b>";
 		for (a=16;a<80;a++)	{
-			if (bits[a]==true) display[1]=display[1]+"1";
-			else display[1]=display[1]+"0";
+			if (bits[a]==true) display[0]=display[0]+"1";
+			else display[0]=display[0]+"0";
 		}
-		display[1]=display[1]+"</b>";
+		display[0]=display[0]+"</b>";
 	}
 	
 	// Handle a Preamble CSBK
