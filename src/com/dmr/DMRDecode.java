4bd5b3797b85fb92e5b9fcd3fc84aa5ace91b341
IanWraith <ianwraith@gmail.com>
2011-01-29 16:22:11 +0000

    Call this build 16.

--- src/com/dmr/DMRDecode.java
+++ src/com/dmr/DMRDecode.java
@@ -41,7 +41,7 @@ public class DMRDecode {
 	private DisplayView display_view;
 	private static DMRDecode theApp;
 	static DisplayFrame window;
-	public String program_version="DMR Decoder V0.00 Build 15";
+	public String program_version="DMR Decoder V0.00 Build 16";
 	public int vertical_scrollbar_value=0;
 	public int horizontal_scrollbar_value=0;
 	private static boolean RUNNING=true;
