9d77bf859ee68117fefe3dd8309eede043043e0b
IanWraith <ianwraith@gmail.com>
2011-03-01 11:24:42 +0000

    Add the basic socket code allowing up to MAXCONNS connections.

--- src/com/dmr/SocketOut.java
+++ src/com/dmr/SocketOut.java
@@ -1,23 +1,18 @@
 package com.dmr;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class SocketOut extends Thread {
-	private boolean run;
-	private boolean connected;
+	private boolean ready;
 	private static int PORT=17887;
+	private static int MAXCONNS=10;
 	private ServerSocket serversocket;
-	private Socket clientSocket=null;
-	private PrintWriter socketOut;
-	private BufferedReader socketIn;
-	
+	private Socket socket[]=new Socket[MAXCONNS];
+	private boolean socketStatus[]=new boolean[MAXCONNS];
+
 	public SocketOut (DMRDecode theApp) {
-    	run=false;
-		connected=false;
+    	ready=false;
     	setPriority(Thread.MIN_PRIORITY);
         start();
         Thread.yield();
@@ -25,10 +20,17 @@ public class SocketOut extends Thread {
 	
 	// Main
     public void run()	{
+    	int next;
     	// Run continously
     	for (;;)	{
-    		if (connected==false) waitForConnection();
-    		
+    		// Wait for a socket connection if the listening socket has been setup
+    		// and there is a free socket available
+    		if ((ready==true)&&(checkForFreeSockets()==true))	{
+    			// Get the index of the next available socket
+    			next=nextFreeSocket();
+    			// Wait for a connection and when it arrives use the free socket
+        		waitForConnection(next);
+     		}
      	}
     }
     
@@ -37,23 +39,40 @@ public class SocketOut extends Thread {
     	try	{
     		serversocket=new ServerSocket(PORT);
     	} catch (Exception e)	{
-    		connected=false;
     		return false;
     	}
-    	connected=true;
+    	ready=true;
     	return true;
     }
     
-    private void waitForConnection()	{
+    // Wait for a connection to the next free socket
+    private void waitForConnection(int n)	{
     	try	{
-    		clientSocket=serversocket.accept();
-    		socketOut=new PrintWriter(clientSocket.getOutputStream(),true);
-            socketIn=new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    		socket[n]=serversocket.accept();
     	} catch (Exception e)	{
-    		connected=false;
     		return;
     	}
-    	connected=true;
+    	socketStatus[n]=true;
+    }
+    
+    // Return the next free socket
+    private int nextFreeSocket()	{
+    	int a;
+    	for (a=0;a<MAXCONNS;a++)	{
+    		if (socketStatus[a]==false) return a;
+    	}
+    	// Return -1 if no sockets are free
+    	return -1;
+    }
+    
+    // Check if there are any sockets free
+    private boolean checkForFreeSockets()	{
+    	int a;
+    	for (a=0;a<MAXCONNS;a++)	{
+    		if (socketStatus[a]==false) return true;
+    	}
+    	// Return false if no sockets are free
+    	return false;
     }
     
 
