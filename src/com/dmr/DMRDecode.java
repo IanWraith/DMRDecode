cfacb6eabc124b8b304116abfeb0ef02c2cab579
IanWraith <ianwraith@gmail.com>
2011-03-11 09:03:58 +0000

    Call this build 30.

--- src/com/dmr/DMRDecode.java
+++ src/com/dmr/DMRDecode.java
@@ -38,7 +38,7 @@ public class DMRDecode {
 	private DisplayView display_view;
 	private static DMRDecode theApp;
 	private static DisplayFrame window;
-	public String program_version="DMR Decoder (Build 29)";
+	public String program_version="DMR Decoder (Build 30)";
 	public int vertical_scrollbar_value=0;
 	public int horizontal_scrollbar_value=0;
 	private static boolean RUNNING=true;
