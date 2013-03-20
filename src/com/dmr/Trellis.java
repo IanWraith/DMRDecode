82002ec263b8be683f95148070dda78ceb520ab0
IanW <ianwraith@gmail.com>
2013-03-20 13:42:46 +0000

    Add a method to extract and deinterleave the dibits ready for the trellis decoding.

--- src/com/dmr/Trellis.java
+++ src/com/dmr/Trellis.java
@@ -7,5 +7,28 @@ public class Trellis {
 			2,3,10,11,18,19,26,27,34,35,42,43,50,51,58,59,66,67,74,75,82,83,90,91,
 			4,5,12,13,20,21,28,29,36,37,44,45,52,53,60,61,68,69,76,77,84,85,92,93,
 			6,7,14,15,22,23,30,31,38,39,46,47,54,55,62,63,70,71,78,79,86,87,94,95};
+	
+	
+	// Extract and deinterleave the dibits
+	private byte[] extractDibits (boolean[] raw)	{
+		int a,index=97;
+		byte rawDibits[]=new byte[98];
+		byte dibits[]=new byte[98];
+		for (a=0;a<196;a=a+2)	{
+			// Set the dibits
+			if (raw[a]==true) rawDibits[index]=2;
+			else rawDibits[index]=0;
+			if (raw[a+1]==true) rawDibits[index]++;
+			// Reduce the index
+			index--;
+		}
+		// Now deinterleave the dibits
+		for (a=0;a<98;a++)	{
+			index=INTERLEAVE[a];
+			dibits[a]=rawDibits[index];
+		}
+		return dibits;
+	}
+	
 
 }
