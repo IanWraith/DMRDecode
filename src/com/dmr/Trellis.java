2688ffcac701151c8034e4757fc45e92495bdc86
IanW <ianwraith@gmail.com>
2013-03-22 13:27:07 +0000

    Add the state transition table for the encoder to the code.

--- src/com/dmr/Trellis.java
+++ src/com/dmr/Trellis.java
@@ -2,11 +2,36 @@ package com.dmr;
 
 public class Trellis {
 	
-	final int INTERLEAVE[]={
+	private final int INTERLEAVE[]={
 			0,1,8,9,16,17,24,25,32,33,40,41,48,49,56,57,64,65,72,73,80,81,88,89,96,97,
 			2,3,10,11,18,19,26,27,34,35,42,43,50,51,58,59,66,67,74,75,82,83,90,91,
 			4,5,12,13,20,21,28,29,36,37,44,45,52,53,60,61,68,69,76,77,84,85,92,93,
 			6,7,14,15,22,23,30,31,38,39,46,47,54,55,62,63,70,71,78,79,86,87,94,95};
+	
+	private final byte STATETABLE[]={
+			0,8,4,12,2,10,6,14,
+			4,12,2,10,6,14,0,8,
+			1,9,5,13,3,11,7,15,
+			5,13,3,11,7,15,1,9,
+			3,11,7,15,1,9,5,13,
+			7,15,1,9,5,13,3,11,
+			2,10,6,14,0,8,4,12,
+			6,14,0,8,4,12,2,10};
+	
+	private byte trellisTable[][]=new byte[8][8];
+	
+	private boolean errorState=false;
+	
+	public Trellis()	{
+		// Load the trellis table from the state table
+		int a,b,c=0;
+		for (a=0;a<8;a++)	{
+			for (b=0;b<8;b++)	{
+				trellisTable[a][b]=STATETABLE[c];
+				c++;
+			}
+		}
+	}
 	
 	
 	// 
@@ -73,6 +98,11 @@ public class Trellis {
 		
 		return constellationPoints;
 	}
-	
+
+
+	public boolean isErrorState() {
+		return errorState;
+	}
+
 
 }
